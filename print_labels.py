d532950ee42039143d8004c87fe989a37224391d
Eric Davis <feralmonkey@gmail.com>
2020-01-30 17:27:07 -0500

    spof the user agent at Like Sew's suggestion

--- print_labels.py
+++ print_labels.py
@@ -155,7 +155,9 @@ def download_report():
     # pp.pprint(c)
 
     url = "https://littleshopofstitches.rainadmin.com/pos-app/customers/download-customers-csv.php"
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0'}
     r = requests.get(url,
+                    headers=headers,
                     params={"type": "edit_custs"},
                     cookies=c)
     logger.debug(r.headers)
